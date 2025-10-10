eeca5f75fe4a6d6ee2b44c206174c54b491b422a
loganphinney <logan.phinney10@gmail.com>
2025-10-09 20:39:02 -0400

    refactor

--- scripts/sysConfiguration.nix
+++ scripts/sysConfiguration.nix
@@ -1,12 +1,9 @@
 { config, pkgs, ... }: {
-  imports = [ ./hardware-configuration.nix ];
-  system.stateVersion = "24.11"; # Did you read the comment?
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  imports = [ ./hardware-configuration.nix <home-manager/nixos> ];
   networking.hostName = "nixos-loganp";
-  networking.networkmanager.enable = true;
-  security.rtkit.enable = true;
+  system.stateVersion = "24.11"; # Did you read the comment?
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = "max";
@@ -35,46 +32,16 @@
     pulse.enable = true;
   };
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [ rocmPackages.rocm-smi amf ];
-  };
-
-  #environment.systemPackages = with pkgs; [ ];
-  programs.steam = {
-    enable = true;
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
-  };
-  programs.zsh = {
-    enable = true;
-    syntaxHighlighting.enable = true;
-    enableBashCompletion = true;
-  };
-
+  networking.networkmanager.enable = true;
+  security.rtkit.enable = true;
   services.openssh.enable = true;
   services.fail2ban.enable = true;
 
-  users.users.loganp = {
-    isNormalUser = true;
-    description = "Logan";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    #packages = with pkgs; [ ];
-    shell = pkgs.zsh;
-  };
-  security.sudo.wheelNeedsPassword = false;
-
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "loganp";
-  };
   services.xserver.enable = true;
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.variant = "";
   services.xserver.excludePackages = [ pkgs.xterm ];
   environment.gnome.excludePackages = with pkgs; [
     snapshot
@@ -100,6 +67,35 @@
     gnome-text-editor
   ];
 
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    enableBashCompletion = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+  users.users.loganp = {
+    isNormalUser = true;
+    description = "Logan";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.zsh;
+  };
+
+  home-manager.users.loganp = { pkgs, ... }: {
+    home.packages = with pkgs; [ nvtopPackages.amd ];
+    home.stateVersion = "25.11";
+  };
+
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "loganp";
+  };
+
   fonts.packages = with pkgs; [
     nerd-fonts.hack
     corefonts
@@ -114,5 +110,11 @@
     enable = true;
     allowedTCPPorts = [ 80 2283 8096 32400 1984 ];
     allowedUDPPorts = [ 80 2283 8096 32400 1984 ];
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [ rocmPackages.rocm-smi amf ];
   };
 }

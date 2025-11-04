bb4d1ee1583b284c6e7ebb66563d7e1270c6be3e
loganphinney <logan.phinney10@gmail.com>
2025-11-04 18:48:48 -0500

    nixfmt-classic -> nixfmt-rfc-style

--- nix/desktopConfiguration.nix
+++ nix/desktopConfiguration.nix
@@ -1,10 +1,13 @@
 { pkgs, lib, ... }:
 let
-  lanzaboote = import (builtins.fetchTarball {
-    url = "https://github.com/nix-community/lanzaboote/archive/master.tar.gz";
-    sha256 = "016pgbh27zjwbzq1li1d4r40b37vwmnz8khh04vspd5gw91szzi1";
-  });
-in {
+  lanzaboote = import (
+    builtins.fetchTarball {
+      url = "https://github.com/nix-community/lanzaboote/archive/master.tar.gz";
+      sha256 = "016pgbh27zjwbzq1li1d4r40b37vwmnz8khh04vspd5gw91szzi1";
+    }
+  );
+in
+{
   imports = [
     ./hardware-configuration.nix
     <home-manager/nixos>
@@ -12,7 +15,10 @@ in {
   ];
   networking.hostName = "nixos-loganp";
   system.stateVersion = "24.11"; # Did you read the comment?
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   nix.optimise.automatic = true;
   nixpkgs.config.allowUnfree = true;
   boot.loader.systemd-boot.enable = lib.mkForce false;
@@ -54,8 +60,16 @@ in {
   virtualisation.docker.enable = true;
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 80 8096 32400 ];
-    allowedUDPPorts = [ 80 8096 32400 ];
+    allowedTCPPorts = [
+      80
+      8096
+      32400
+    ];
+    allowedUDPPorts = [
+      80
+      8096
+      32400
+    ];
   };
   hardware.graphics = {
     enable = true;
@@ -65,7 +79,11 @@ in {
   users.users.loganp = {
     isNormalUser = true;
     description = "Logan";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+    ];
     shell = pkgs.zsh;
   };
   services.displayManager.autoLogin.enable = true;
@@ -145,138 +163,140 @@ in {
     inter
   ];
 
-  home-manager.users.loganp = { pkgs, ... }: {
-    home.stateVersion = "25.11";
-    #home.packages = with pkgs; [ ];
-    programs.zsh = {
-      enable = true;
-      initContent = "PROMPT='%B%F{green}[%1~]%f%b%F{grey}%#%f '";
-      shellAliases = {
-        ".." = "cd ../";
-        "~" = "cd ~/";
-        cl = "clear";
-        ls = "eza";
-        la = "eza -a";
-        ll = "eza -l";
-        l1 = "eza -1";
-        tree = "eza -T";
-        nv = "nvim";
-        nvsu = "sudo -E nvim";
-        bat =
-          "bat --color=always --theme=ansi --style=-numbers,-header,+changes";
-        dcdu = "docker compose down; docker compose up -d";
-        fzf =
-          "fzf --style full --preview 'bat --color=always --theme=ansi --style=-numbers,-header,+changes {}'";
-        lava = "lavat -c black -k magenta -s 3";
-        cmatrix = "cmatrix -C magenta";
-        nixsysup = "sudo nixos-rebuild switch --upgrade";
-        nixsysed = "nvsu /etc/nixos/configuration.nix";
-        nixlsgens =
-          "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
-        nixusrup = "nix profile upgrade nix/loganp --verbose";
+  home-manager.users.loganp =
+    { pkgs, ... }:
+    {
+      home.stateVersion = "25.11";
+      #home.packages = with pkgs; [ ];
+      programs.zsh = {
+        enable = true;
+        initContent = "PROMPT='%B%F{green}[%1~]%f%b%F{grey}%#%f '";
+        shellAliases = {
+          ".." = "cd ../";
+          "~" = "cd ~/";
+          cl = "clear";
+          ls = "eza";
+          la = "eza -a";
+          ll = "eza -l";
+          l1 = "eza -1";
+          tree = "eza -T";
+          nv = "nvim";
+          nvsu = "sudo -E nvim";
+          bat = "bat --color=always --theme=ansi --style=-numbers,-header,+changes";
+          dcdu = "docker compose down; docker compose up -d";
+          fzf = "fzf --style full --preview 'bat --color=always --theme=ansi --style=-numbers,-header,+changes {}'";
+          lava = "lavat -c black -k magenta -s 3";
+          cmatrix = "cmatrix -C magenta";
+          nixsysup = "sudo nixos-rebuild switch --upgrade";
+          nixsysed = "nvsu /etc/nixos/configuration.nix";
+          nixlsgens = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
+          nixusrup = "nix profile upgrade nix/loganp --verbose";
+        };
+        plugins = [
+          {
+            name = "fast-syntax-highlighting";
+            src = pkgs.zsh-fast-syntax-highlighting;
+            file = "share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh";
+          }
+        ];
       };
-      plugins = [{
-        name = "fast-syntax-highlighting";
-        src = pkgs.zsh-fast-syntax-highlighting;
-        file =
-          "share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh";
-      }];
-    };
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    programs.tmux = {
-      enable = true;
-      baseIndex = 1;
-      mouse = true;
-      focusEvents = true;
-      clock24 = true;
-      shortcut = "s";
-      extraConfig = ''
-        set-option -g status-position top
-        set -g renumber-windows on
-        set -g pane-border-lines "single"
-        set -g pane-active-border-style "fg=#3e8fb0"
-        bind-key "|" split-window -h -c "#{pane_current_path}"
-        bind-key "\\" split-window -fh -c "#{pane_current_path}"
-        bind-key "-" split-window -v -c "#{pane_current_path}"
-        bind-key "_" split-window -fv -c "#{pane_current_path}"
-      '';
-      plugins = with pkgs.tmuxPlugins; [
-        {
-          plugin = (mkTmuxPlugin {
-            pluginName = "rose-pine-tmux";
-            version = "unstable-2025-08-26";
-            src = pkgs.fetchFromGitHub {
-              owner = "rose-pine";
-              repo = "tmux";
-              rev = "009800e5c892c0e75de648881f8ba09a90c145b0";
-              hash = "sha256-OJMBCZwqrEu2DTlojqQ3pIp2sfjIzT9ORw0ajVgZ8vo=";
-            };
-            rtpFilePath = "rose-pine.tmux";
-          });
-          extraConfig = ''
-            set -g @rose_pine_variant 'main'
-            set -g @rose_pine_disable_active_window_menu 'on'
-            set -g @rose_pine_show_current_program 'on'
-            set -g @rose_pine_host 'on'
-            set -g @rose_pine_date_time '%b-%d-%Y %H:%M:%S'
-            set -g @rose_pine_user 'on' 
-            set -g @rose_pine_directory 'on'
-            set -g @rose_pine_right_separator ' '
-            set -g @rose_pine_status_right_prepend_section '#{cpu_icon}#{cpu_percentage} ' 
-          '';
-        }
-        cpu
-      ];
-    };
-    programs.kitty = {
-      enable = true;
-      font.name = "Hack Nerd Font Mono";
-      font.size = 11;
-      settings = {
-        linux_display_server = "x11";
-        sync_to_monitor = false;
-        cursor_shape = "beam";
-        cursor_trail = 1;
-        cursor_trail_decay = "0.1 0.3";
-        enable_audio_bell = false;
-        tab_bar_style = "slant";
-        tab_bar_align = "right";
-        remember_window_size = false;
-        initial_window_width = "120c";
-        initial_window_height = "40c";
-        foreground = "#e0def4";
-        background = "#191724";
-        selection_foreground = "#e0def4";
-        selection_background = "#403d52";
-        cursor = "#e0def4";
-        cursor_text_color = "#e0def4";
-        url_color = "#c4a7e7";
-        active_tab_foreground = "#e0def4";
-        active_tab_background = "#26233a";
-        inactive_tab_foreground = "#6e6a86";
-        inactive_tab_background = "#191724";
-        active_border_color = " #3e8fb0";
-        inactive_border_color = "#403d52";
-        color0 = "#26233a";
-        color8 = "#6e6a86";
-        color1 = "#eb6f92";
-        color9 = "#eb6f92";
-        color2 = "#3e8fb0";
-        color10 = "#3e8fb0";
-        color3 = "#f6c177";
-        color11 = "#f6c177";
-        color4 = "#9ccfd8";
-        color12 = "#9ccfd8";
-        color5 = "#c4a7e7";
-        color13 = "#c4a7e7";
-        color6 = "#ebbcba";
-        color14 = "#ebbcba";
-        color7 = "#e0def4";
-        color15 = "#e0def4";
+      programs.fzf = {
+        enable = true;
+        enableZshIntegration = true;
+      };
+      programs.tmux = {
+        enable = true;
+        baseIndex = 1;
+        mouse = true;
+        focusEvents = true;
+        clock24 = true;
+        shortcut = "s";
+        extraConfig = ''
+          set-option -g status-position top
+          set -g renumber-windows on
+          set -g pane-border-lines "single"
+          set -g pane-active-border-style "fg=#3e8fb0"
+          bind-key "|" split-window -h -c "#{pane_current_path}"
+          bind-key "\\" split-window -fh -c "#{pane_current_path}"
+          bind-key "-" split-window -v -c "#{pane_current_path}"
+          bind-key "_" split-window -fv -c "#{pane_current_path}"
+        '';
+        plugins = with pkgs.tmuxPlugins; [
+          {
+            plugin = (
+              mkTmuxPlugin {
+                pluginName = "rose-pine-tmux";
+                version = "unstable-2025-08-26";
+                src = pkgs.fetchFromGitHub {
+                  owner = "rose-pine";
+                  repo = "tmux";
+                  rev = "009800e5c892c0e75de648881f8ba09a90c145b0";
+                  hash = "sha256-OJMBCZwqrEu2DTlojqQ3pIp2sfjIzT9ORw0ajVgZ8vo=";
+                };
+                rtpFilePath = "rose-pine.tmux";
+              }
+            );
+            extraConfig = ''
+              set -g @rose_pine_variant 'main'
+              set -g @rose_pine_disable_active_window_menu 'on'
+              set -g @rose_pine_show_current_program 'on'
+              set -g @rose_pine_host 'on'
+              set -g @rose_pine_date_time '%b-%d-%Y %H:%M:%S'
+              set -g @rose_pine_user 'on' 
+              set -g @rose_pine_directory 'on'
+              set -g @rose_pine_right_separator ' '
+              set -g @rose_pine_status_right_prepend_section '#{cpu_icon}#{cpu_percentage} ' 
+            '';
+          }
+          cpu
+        ];
+      };
+      programs.kitty = {
+        enable = true;
+        font.name = "Hack Nerd Font Mono";
+        font.size = 11;
+        settings = {
+          linux_display_server = "x11";
+          sync_to_monitor = false;
+          cursor_shape = "beam";
+          cursor_trail = 1;
+          cursor_trail_decay = "0.1 0.3";
+          enable_audio_bell = false;
+          tab_bar_style = "slant";
+          tab_bar_align = "right";
+          remember_window_size = false;
+          initial_window_width = "120c";
+          initial_window_height = "40c";
+          foreground = "#e0def4";
+          background = "#191724";
+          selection_foreground = "#e0def4";
+          selection_background = "#403d52";
+          cursor = "#e0def4";
+          cursor_text_color = "#e0def4";
+          url_color = "#c4a7e7";
+          active_tab_foreground = "#e0def4";
+          active_tab_background = "#26233a";
+          inactive_tab_foreground = "#6e6a86";
+          inactive_tab_background = "#191724";
+          active_border_color = " #3e8fb0";
+          inactive_border_color = "#403d52";
+          color0 = "#26233a";
+          color8 = "#6e6a86";
+          color1 = "#eb6f92";
+          color9 = "#eb6f92";
+          color2 = "#3e8fb0";
+          color10 = "#3e8fb0";
+          color3 = "#f6c177";
+          color11 = "#f6c177";
+          color4 = "#9ccfd8";
+          color12 = "#9ccfd8";
+          color5 = "#c4a7e7";
+          color13 = "#c4a7e7";
+          color6 = "#ebbcba";
+          color14 = "#ebbcba";
+          color7 = "#e0def4";
+          color15 = "#e0def4";
+        };
       };
     };
-  };
 }
--- nix/loganp/flake.nix
+++ nix/loganp/flake.nix
@@ -1,52 +1,58 @@
 {
   description = "loganp's nix packages";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-    in with pkgs;
-    buildEnv {
-      name = "loganp-packages";
-      paths = [
-        #neovim stuff
-        neovim
-        tree-sitter
-        gcc
-        luajitPackages.jsregexp
-        bash-completion
-        shellcheck
-        shfmt
-        bash-language-server
-        lua-language-server
-        typescript-language-server
-        nixd
-        nixfmt-classic
-        perl540Packages.PLS
-        vscode-langservers-extracted
-        yaml-language-server
-        #for fun
-        lavat
-        pokeget-rs
-        #GNOME
-        gnome-tweaks
-        xdg-terminal-exec
-        rose-pine-cursor
-        mission-center
-        gnomeExtensions.open-bar
-        gnomeExtensions.hide-top-bar
-        gnomeExtensions.user-themes
-        gnomeExtensions.vitals
-        gnomeExtensions.wallpaper-slideshow
-        gnomeExtensions.color-picker
-        gnomeExtensions.executor
-        #GUI apps
-        qbittorrent
-        prismlauncher
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages.x86_64-linux.default =
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+        in
+        with pkgs;
+        buildEnv {
+          name = "loganp-packages";
+          paths = [
+            #neovim stuff
+            vim
+            neovim
+            tree-sitter
+            gcc
+            luajitPackages.jsregexp
+            bash-completion
+            shellcheck
+            shfmt
+            bash-language-server
+            lua-language-server
+            typescript-language-server
+            nixd
+            nixfmt-rfc-style
+            perl540Packages.PLS
+            vscode-langservers-extracted
+            yaml-language-server
+            #for fun
+            lavat
+            pokeget-rs
+            #GNOME
+            gnome-tweaks
+            xdg-terminal-exec
+            rose-pine-cursor
+            mission-center
+            gnomeExtensions.open-bar
+            gnomeExtensions.hide-top-bar
+            gnomeExtensions.user-themes
+            gnomeExtensions.vitals
+            gnomeExtensions.wallpaper-slideshow
+            gnomeExtensions.color-picker
+            gnomeExtensions.executor
+            #GUI apps
+            qbittorrent
+            prismlauncher
+          ];
+        };
+    };
 }
-

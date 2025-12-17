05ad8537b97b4ae6e4ca9ca3078f664a9912a074
loganphinney <logan.phinney10@gmail.com>
2025-12-17 17:43:26 -0500

    update

--- nix/ansible/flake.nix
+++ nix/ansible/flake.nix
@@ -2,34 +2,45 @@
   description = "packages for ansible";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-
-      ansible-language-server = pkgs.writeShellScriptBin "ansible-language-server" ''
-        exec ${pkgs.nodejs}/bin/npx @ansible/ansible-language-server@1.2.3 "$@"
-      '';
-    in
     {
-      packages.${system}.default =
-        with pkgs;
-        buildEnv {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        ansible-language-server = pkgs.writeShellScriptBin "ansible-language-server" ''
+          exec ${pkgs.nodejs_24}/bin/npx --yes @ansible/ansible-language-server@1.2.3 "$@"
+        '';
+      in
+      {
+        packages.default = pkgs.buildEnv {
           name = "loganp-packages";
-          paths = [
+          paths = with pkgs; [
             python314
             pyenv
             ansible
-            ansible-language-server
             ansible-lint
             yamllint
             nodejs_24
+            ansible-language-server
           ];
         };
-    };
+        devShells.default = pkgs.mkShell {
+          packages = [ self.packages.${system}.default ];
+          shellHook = ''
+            export PATH="$PATH:$(pwd)/node_modules/.bin"
+            echo "Ansible Dev Environment Loaded"
+          '';
+        };
+      }
+    );
 }

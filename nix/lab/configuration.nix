1b7129c5bdb11e716635d3083133edec54e992ee
loganphinney <logan.phinney10@gmail.com>
2026-04-03 23:35:24 -0400

    added a monitoring stack

--- nix/lab/configuration.nix
+++ nix/lab/configuration.nix
@@ -27,7 +27,6 @@
   environment.systemPackages = with pkgs; [
     tmux
     gcc
-    neovim
     git
     wget
     curl
@@ -55,6 +54,7 @@
   networking.firewall.allowedTCPPorts = [
     80
     443
+    3000
   ];
   networking.firewall.allowedUDPPorts = [ 443 ];
   services.caddy = {
@@ -76,6 +76,119 @@
       enable = true;
       openFirewall = true;
       host = "0.0.0.0";
+    };
+    grafana = {
+      enable = true;
+      settings.security.secret_key = "77764cbb7ee9e979b8bae4b843808566";
+      settings.server = {
+        http_addr = "0.0.0.0";
+        http_port = 3000;
+      };
+      provision = {
+        enable = true;
+        datasources.settings.datasources = [
+          {
+            name = "Loki";
+            type = "loki";
+            access = "proxy";
+            url = "http://127.0.0.1:3100";
+            isDefault = true;
+          }
+          {
+            name = "Prometheus";
+            type = "prometheus";
+            access = "proxy";
+            url = "http://127.0.0.1:9090";
+          }
+        ];
+      };
+    };
+    prometheus = {
+      enable = true;
+      exporters.node = {
+        enable = true;
+        port = 9100;
+      };
+      scrapeConfigs = [
+        {
+          job_name = "node";
+          static_configs = [
+            {
+              targets = [ "127.0.0.1:9100" ];
+            }
+          ];
+        }
+      ];
+    };
+    loki = {
+      enable = true;
+      configuration = {
+        auth_enabled = false;
+        server = {
+          http_listen_port = 3100;
+        };
+        common = {
+          ring = {
+            instance_addr = "127.0.0.1";
+            kvstore.store = "inmemory";
+          };
+          replication_factor = 1;
+          path_prefix = "/tmp/loki";
+        };
+        schema_config.configs = [
+          {
+            from = "2020-05-15";
+            store = "tsdb";
+            object_store = "filesystem";
+            schema = "v13";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }
+        ];
+        storage_config.filesystem = {
+          directory = "/tmp/loki/chunks";
+        };
+      };
+    };
+    fluent-bit = {
+      enable = true;
+      settings = {
+        service = {
+          flush = 1;
+          log_level = "info";
+        };
+        pipeline = {
+          inputs = [
+            {
+              name = "systemd";
+              tag = "services.jellyfin";
+              systemd_filter = "_SYSTEMD_UNIT=jellyfin.service";
+            }
+            {
+              name = "systemd";
+              tag = "services.immich";
+              systemd_filter = "_SYSTEMD_UNIT=immich.service";
+            }
+            {
+              name = "systemd";
+              tag = "services.grafana";
+              systemd_filter = "_SYSTEMD_UNIT=grafana.service";
+            }
+          ];
+          outputs = [
+            {
+              name = "loki";
+              match = "*";
+              host = "127.0.0.1";
+              port = 3100;
+              labels = [ "job=fluent-bit" ];
+              line_format = "json";
+            }
+          ];
+        };
+      };
     };
   };
   users.groups.media = {

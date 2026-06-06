738bfcdc0c00f6268200749ada12bf8261932559
loganphinney <logan.phinney10@gmail.com>
2026-06-06 14:56:50 -0400

    tree-sitter

--- .config/nvim/lua/plugins/saga.lua
+++ .config/nvim/lua/plugins/saga.lua
@@ -1,6 +1,5 @@
 vim.pack.add({
     'https://github.com/nvimdev/lspsaga.nvim',
-    'https://github.com/nvim-treesitter/nvim-treesitter',
     'https://github.com/nvim-tree/nvim-web-devicons'
 })
 require('lspsaga').setup({
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,23 @@
---[[
-return {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-context' },
-    config = function()
-        require('nvim-treesitter').setup {
-            sync_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            }
-        }
-        require('nvim-treesitter').install {
-            'bash',
-            'lua',
-            'perl',
-            'python',
-            'vim',
-            'vimdoc',
-            'query',
-            'markdown',
-            'markdown_inline',
-            'regex',
-            'json',
-            'yaml',
-            'nix',
-            'html',
-            'hcl',
-        }
-    end
+vim.pack.add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-treesitter/nvim-treesitter-context'
+})
+require('nvim-treesitter').setup {
+    sync_install = false,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+    }
 }
-]] --
+require('nvim-treesitter').install {
+    'json',
+    'yaml',
+    'ini',
+    'regex',
+    'html',
+    'bash',
+    'perl',
+    'python',
+    'nix',
+    'typescript'
+}
